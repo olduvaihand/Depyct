d15aafd2f28ddfcde3e5ba256fcf2cfe4fd25ace
Benjamin Trofatter <ben@secventures.com>
2012-11-13 17:27:06 -0600

    Added ImageSize helper class
    Added ImageSize helper class to depyct/image.py
    Added tests for bad values and results of calling repr on ImageSize
        instances

--- lib/depyct/image.py
+++ lib/depyct/image.py
@@ -5,10 +5,39 @@
 # http://www.opensource.org/licenses/mit-license.php
 """
 """
+from collections import namedtuple
+from operator import index
 
-class ImageMode(str):
+from .image_mode import *
+
+
+__all__ = ["ImageSize", "ImageMixin", "Image"]
+
+
+class ImageSize(namedtuple("_ImageSize", "width height")):
     """
+
     """
+
+    def __init__(self, width, height):
+        if width <= 0:
+            raise ValueError("width must be greater than 0")
+        if height <= 0:
+            raise ValueError("height must be greater than 0")
+        try:
+            width = index(width)
+        except TypeError:
+            raise TypeError("width must be of integral value or provide "
+                            "an __index__ method")
+        try:
+            height = index(height)
+        except TypeError:
+            raise TypeError("height must be of integral value or provide "
+                            "an __index__ method")
+        super(ImageSize, self).__init__(width, height)
+
+    def __repr__(self):
+        return "ImageSize(width=%d, height=%d)" % (self.width, self.height)
 
 
 class ImageMixin(object):
---
+++ test/unit_tests/test_image_size.py
@@ -0,0 +1,25 @@
+# test/unit_tests/test_image_size.py
+# Copyright (c) 2012 the Depyct authors and contributors <see AUTHORS>
+#
+# This module is part of Depyct and is released under the MIT License:
+# http://www.opensource.org/licenses/mit-license.php
+
+from depyct.testing import DepyctUnitTest
+from depyct.image import ImageSize
+
+
+class ImageSizeTest(DepyctUnitTest):
+
+    def test___repr__(self):
+        im_size = ImageSize(10, 10)
+        self.assertEquals(repr(im_size), "ImageSize(width=10, height=10)")
+
+    def test_bad_input(self):
+        for w, h in ((0, 10), (-1, 10), (0, 0), (-1, -1), (10, 0), (10, -1)):
+            with self.assertRaises(ValueError):
+                ImageSize(w, h)
+
+        for w, h in ((1., 1), (1, 1.), (1., 1.), ('a', 1), (1, 'a'), 'aa'):
+            with self.assertRaises(TypeError):
+                ImageSize(w, h)
+
