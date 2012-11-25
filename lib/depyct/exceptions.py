aa48352b1c1a1ce05686c42bb63fa1fa129c971f
Benjamin Trofatter <ben@secventures.com>
2012-11-25 14:40:34 -0600

    Added depyct.exceptions module.

---
+++ lib/depyct/exceptions.py
@@ -0,0 +1,12 @@
+# depyct/exceptions.py
+# Copyright (c) 2012 the Depyct authors and contributors <see AUTHORS>
+#
+# This module is part of Depyct and is released under the MIT License:
+# http://www.opensource.org/licenses/mit-license.php
+"""
+
+"""
+
+
+class DepyctError(Exception):
+    pass
