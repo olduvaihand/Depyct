6feb51559139b3e6dc4a224642a8a418e807064c
Benjamin Trofatter <ben@secventures.com>
2012-12-02 21:03:29 -0600

    Added readonly_property to util.py

--- lib/depyct/util.py
+++ lib/depyct/util.py
@@ -12,3 +12,21 @@ py27 = sys.version_info == (2, 7)
 py3k = sys.version_info >= (3, 0)
 jython = sys.platform.startswith("java")
 pypy = sys.platform.startswith("pypy")
+
+
+class readonly_property(object):
+
+    def __init__(self, fget):
+        self.name = fget.__name__
+        self.fget = fget
+
+    def __get__(self, obj, typename=None):
+        if obj is None:
+            return self
+        return self.fget(obj)
+
+    def __set__(self, obj, value):
+        raise TypeError("{} is a read-only value.".format(self.name))
+
+    def __delete__(self, obj):
+        raise TypeError("{} is a read-only value.".format(self.name))
