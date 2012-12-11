5104262e4a5ba134a5d5badb267687e7a37a3157
Ben Trofatter <bentrofatter@gmail.com>
2012-12-11 00:54:44 -0600

    Added stubs for plugin testing

---
+++ test/unit_tests/test_io/test_plugins/test_png.py
@@ -0,0 +1,5 @@
+# test/unit_tests/test_io/test_plugins/test_png.py
+# Copyright (c) 2012 the Depyct authors and contributors <see AUTHORS>
+#
+# This module is part of Depyct and is released under the MIT License:
+# http://www.opensource.org/licenses/mit-license.php
