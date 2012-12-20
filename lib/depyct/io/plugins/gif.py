1d8010d29aee048a1d0fe027bd72a5866dde9f5a
Benjamin Trofatter <ben@secventures.com>
2012-12-19 21:01:36 -0600

    Added GIFStruct to simplify reading in blocks

--- lib/depyct/io/plugins/gif.py
+++ lib/depyct/io/plugins/gif.py
@@ -5,14 +5,22 @@ from depyct.image.mode import RGB
 from depyct.io.format import FormatBase
 
 
-class RGBTriple(ctypes.LittleEndianStructure):
+class GIFStruct(ctypes.LittleEndianStructure):
+    _pack_ = 1
+
+    @classmethod
+    def load(cls, fp):
+        return cls.from_buffer_copy(fp.read(ctypes.sizeof(cls)))
+
+
+class RGBTriple(GIFStruct):
     _pack_ = 1
     _fields_ = [("r", ctypes.c_byte),
                 ("g", ctypes.c_byte),
                 ("b", ctypes.c_byte)]
 
 
-class LogicalScreenDescriptor(ctypes.LittleEndianStructure):
+class LogicalScreenDescriptor(GIFStruct):
     _pack_ = 1
     _fields_ = [("width", ctypes.c_uint16),
                 ("height", ctypes.c_uint16),
@@ -24,7 +32,7 @@ class LogicalScreenDescriptor(ctypes.LittleEndianStructure):
                 ("aspect_ratio", ctypes.c_byte)]
 
 
-class ImageDescriptor(ctypes.LittleEndianStructure):
+class ImageDescriptor(GIFStruct):
     _pack_ = 1
     _fields_ = [("x", ctypes.c_uint16),
                 ("y", ctypes.c_uint16),
@@ -37,7 +45,7 @@ class ImageDescriptor(ctypes.LittleEndianStructure):
                 ("local_color_table_size", ctypes.c_uint8, 3)]
 
 
-class GraphicControlExtension(ctypes.LittleEndianStructure):
+class GraphicControlExtension(GIFStruct):
     _pack_ = 1
     _fields_ = [("size", ctypes.c_byte),
                 ("delay", ctypes.c_uint16),
@@ -49,7 +57,7 @@ class GraphicControlExtension(ctypes.LittleEndianStructure):
                 ("terminator", ctypes.c_byte)]
 
 
-class PlainTextExtension(ctypes.LittleEndianStructure):
+class PlainTextExtension(GIFStruct):
     _pack_ = 1
     _fields_ = [("size", ctypes.c_byte),
                 ("x", ctypes.c_uint16),
@@ -62,7 +70,7 @@ class PlainTextExtension(ctypes.LittleEndianStructure):
                 ("background_color_index", ctypes.c_byte)]
 
 
-class ApplicationExtension(ctypes.LittleEndianStructure):
+class ApplicationExtension(GIFStruct):
     _pack_ = 1
     _fields_ = [("size", ctypes.c_byte),
                 ("identifier", ctypes.c_byte * 8),
@@ -87,24 +95,27 @@ class GIFFormat(FormatBase):
     extensions = ("gif", "gfa", "giff")
     mimetypes = ("image/gif",)
 
-    def read(self):
-        self.global_color_table = None
-        self.header = self.fp.read(6)
-        self.version = self.header[3:]
-        lsd = self.fp.read(7)
-        self.logical_screen = LogicalScreenDescriptor.from_buffer_copy(lsd)
-        if self.logical_screen.global_color_table_flag:
-            # read global color table
-            size = self.logical_screen.global_color_table_size
-            table_bytes = 3 * 2**(size+1)
-            GlobalColorTable = RGBTriple * size
-            self.gct = GlobalColorTable.from_buffer_copy(self.fp.read(table_bytes))
-        self.size = self.logical_screen.width, self.logical_screen.height
-        self.im = self.image_cls(RGB, size=self.size)
+    def __init__(self, image_cls, **config):
+        super(GIFFormat, self).__init__(image_cls, **config)
+        self.graphic_controls = []
+        self.global_color_table = []
         self.comments = []
         self.plain_text = []
         self.image_data = []
         self.application_data = []
+
+    def read(self):
+        self.header = self.fp.read(6)
+        self.version = self.header[3:]
+        self.logical_screen = LogicalScreenDescriptor.load(self.fp)
+        if self.logical_screen.global_color_table_flag:
+            # read global color table
+            size = self.logical_screen.global_color_table_size
+            GlobalColorTable = type("GlobalColorTable", (GIFStruct,),
+                                  {"_fields_": [("colors", RGBTriple * 2**(size+1))]})
+            self.global_color_table = GlobalColorTable.load(self.fp)
+        self.size = self.logical_screen.width, self.logical_screen.height
+        self.im = self.image_cls(RGB, size=self.size)
         self.load()
         return self
 
@@ -115,9 +126,8 @@ class GIFFormat(FormatBase):
             if block_intro == EXTENSION:
                 label = self.fp.read(1)
                 if label == GRAPHIC_CONTROL:
-                    size = ord(self.fp.read(1))
-                    # not certain this is actually useful
-                    self.fp.read(size + 1)
+                    gce = GraphicControlExtension.load(self.fp)
+                    self.graphic_controls.append(gce)
                 elif label == COMMENT:
                     comment = []
                     while True:
@@ -125,44 +135,39 @@ class GIFFormat(FormatBase):
                         if size == 0:
                             break
                         comment.append(self.fp.read(size))
-                    # FIXME: do something with this?
                     comment = b"".join(comment)
                     self.comments.append(comment)
                 elif label == PLAIN_TEXT:
-                    pte = PlainTextExtension.from_buffer_copy(
-                            self.fp.read(ctypes.sizeof(PlainTextExtension)))
+                    pte = PlainTextExtension.load(self.fp)
                     text = []
                     while True:
                         size = ord(self.fp.read(1))
                         if size == 0:
                             break
                         text.append(self.fp.read(size))
-                    # FIXME: do something with this?
                     text = b"".join(text)
                     self.plain_text.append(text)
                 elif label == APPLICATION:
-                    ae = ApplicationExtension.from_buffer_copy(
-                            self.fp.read(ctypes.sizeof(ApplicationExtension)))
+                    ae = ApplicationExtension.load(self.fp)
                     application_data = []
                     while True:
                         size = ord(self.fp.read(1))
                         if size == 0:
                             break
-                        # FIXME: do something with this?
                         application_data.append(self.fp.read(size))
                     application_data = b"".join(application_data)
                     self.application_data.append(application_data)
                 else:
-                    raise IOError("Unrecognized extension block label {} {}.".format(block_intro, label))
+                    raise IOError("Unrecognized extension block label at "
+                                  "offset {}: {} {}.".format(
+                                  self.fp.tell(), block_intro, label))
             elif block_intro == IMAGE_SEPARATOR:
-                image_descriptor = ImageDescriptor.from_buffer_copy(
-                        self.fp.read(ctypes.sizeof(ImageDescriptor)))
+                image_descriptor = ImageDescriptor.load(self.fp)
                 if image_descriptor.local_color_table_flag:
                     size = self.image_descriptor.local_color_table_size
-                    table_bytes = 3 * 2**(size+1)
-                    LocalColorTable = RGBTriple * size
-                    color_table = LocalColorTable.from_buffer_copy(
-                                      self.fp.read(table_bytes))
+                    LocalColorTable = type("LocalColorTable", (GIFStruct,),
+                                  {"_fields_": [("colors", RGBTriple * 2**(size+1))]})
+                    self.local_color_table = LocalColorTable.load(self.fp)
                 lzw_minimum_code_size = self.fp.read(1)
                 image_data = []
                 while True:
@@ -176,7 +181,8 @@ class GIFFormat(FormatBase):
             elif block_intro == TRAILER:
                 break
             else:
-                raise IOError("Unrecognized block {}.".format(block_intro))
+                raise IOError("Unrecognized block at {}: {}.".format(
+                              self.fp.tell(), block_intro))
 
     def write(self):
         raise NotImplementedError
